bc0d7bea0b10bae1cd1429635eab6c14663be1bd
Honya <training@nwegar.com>
2023-12-26 15:12:14 +0300

    third commit

--- src/Pages/Cakes/Cakes.jsx
+++ src/Pages/Cakes/Cakes.jsx
@@ -1,64 +1,68 @@
 import React from "react";
 import "./Cakes.css";
 import { Link } from "react-router-dom";
-import useTitle from './../../hooks/useTitle';
+import useTitle from "./../../hooks/useTitle";
 function Cakes() {
-  useTitle("Cakes")
+  useTitle("Cakes");
   const cakes = [
     {
       img: require("../../img/cake4.jpg"),
       name: "fruite cake",
       price: "$10",
-      orginalPrice:"$16.33",
+      orginalPrice: "$16.33",
       btn: "add to cart",
     },
     {
       img: require("../../img/cake4.jpg"),
       name: "fruite cake",
       price: "$10",
-      orginalPrice:"$15",
+      orginalPrice: "$15",
       btn: "add to cart",
     },
     {
       img: require("../../img/cake4.jpg"),
       name: "fruite cake",
       price: "$10",
-      orginalPrice:"$14.33",
+      orginalPrice: "$14.33",
       btn: "add to cart",
     },
     {
       img: require("../../img/cake4.jpg"),
       name: "fruite cake",
       price: "$10",
-      orginalPrice:"$15.33",
+      orginalPrice: "$15.33",
       btn: "add to cart",
     },
     {
       img: require("../../img/cake4.jpg"),
       name: "fruite cake",
       price: "$10",
-      orginalPrice:"$13",
+      orginalPrice: "$13",
       btn: "add to cart",
     },
     {
       img: require("../../img/cake4.jpg"),
       name: "fruite cake",
       price: "$10",
-      orginalPrice:"$15.33",
+      orginalPrice: "$15.33",
       btn: "add to cart",
     },
   ];
   return (
-    <section className=" cakes" id="cakes">
+    <section className="cakes" id="cakes">
       <h3 className="page-title">
         our <span>Cakes</span>
       </h3>
       <div className="wrapper">
         {cakes.map((cake) => (
           <div className="cake-card">
-            <img src={cake.img} alt="cakes" />
-            <h3 >{cake.name}</h3>
-            <div className="price">{cake.price} <span>{cake.orginalPrice}</span> </div>
+            <div className="img">
+              <img src={cake.img} alt="cakes" />{" "}
+            </div>
+            <h3>{cake.name}</h3>
+            <div className="price">
+              {cake.price} <span>{cake.orginalPrice}</span>{" "}
+            </div>
             <Link to="" className="btn">
               {cake.btn}
             </Link>
